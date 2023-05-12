fff2aa79fe008e1952eabb981b037d091ca4a3f0
rexode <santiago.garcia.castillo@outlook.com>
2023-05-12 20:05:46 +0200

    first version

--- src/Components/Home/Doyou.js
+++ src/Components/Home/Doyou.js
@@ -1,52 +1,72 @@
-import React from 'react'
-import {Button, Container ,Box ,Grid ,List, ListItem ,Hidden,Typography} from '@mui/material';
-import Whitepaper from '../../Assets/images/Whitepaper.pdf'
+import React from "react";
+import {
+  Button,
+  Container,
+  Box,
+  Grid,
+  List,
+  ListItem,
+  Hidden,
+  Typography,
+} from "@mui/material";
+import Whitepaper from "./Whitepaper.pdf";
 
 export default function Doyou() {
   return (
     <>
-    <Container>
-        <div className='doyourdr' >
-            <div className=' doyou-inner ' >
-            <Grid container spacing={4}  >
-                <Grid  item xs={12} lg={4}  className="line-aftr" >
-                <Typography variant="h1" gutterBottom  className="about-text" >
-                Do you want to know more about us?
-      </Typography>
-                </Grid>
-                <Grid item xs={12} lg={8} >
-
-                    <Grid container justifyContent="center">
-                    <Grid item xs={6} sm={6} md={4} >
-                        <Box textAlign="center" >
-                        <Button color="primary" variant='contained' href={Whitepaper}  className='Whitepaper'  >
+      <Container>
+        <div className="doyourdr">
+          <div className=" doyou-inner ">
+            <Grid container spacing={4}>
+              <Grid item xs={12} lg={4} className="line-aftr">
+                <Typography variant="h1" gutterBottom className="about-text">
+                  Do you want to know more about us?
+                </Typography>
+              </Grid>
+              <Grid item xs={12} lg={8}>
+                <Grid container justifyContent="center">
+                  <Grid item xs={6} sm={6} md={4}>
+                    <Box textAlign="center">
+                      <Button
+                        color="primary"
+                        variant="contained"
+                        href={Whitepaper}
+                        className="Whitepaper"
+                      >
                         Whitepaper
-    </Button>
-                        </Box>
-                    </Grid>
-                    <Grid item xs={6} sm={6} md={4} >
-                        <Box textAlign="center" >
-                        <Button color="primary" variant='contained' className='twitter' href="https://twitter.com/criptoluckyreal"  >
+                      </Button>
+                    </Box>
+                  </Grid>
+                  <Grid item xs={6} sm={6} md={4}>
+                    <Box textAlign="center">
+                      <Button
+                        color="primary"
+                        variant="contained"
+                        className="twitter"
+                        href="https://twitter.com/criptoluckyreal"
+                      >
                         Twitter
-    </Button>
-                        </Box>
-                    </Grid>
-                    <Grid item xs={6} sm={6} md={4} >
-                        <Box textAlign="center" >
-                        <Button color="primary" variant='contained' className='Whitepaper-2' href="#roadmap"  >
+                      </Button>
+                    </Box>
+                  </Grid>
+                  <Grid item xs={6} sm={6} md={4}>
+                    <Box textAlign="center">
+                      <Button
+                        color="primary"
+                        variant="contained"
+                        className="Whitepaper-2"
+                        href="#roadmap"
+                      >
                         Roadmap
-    </Button>
-                        </Box>
-                    </Grid>
-
-                    </Grid>
-
+                      </Button>
+                    </Box>
+                  </Grid>
                 </Grid>
+              </Grid>
             </Grid>
-            </div>
+          </div>
         </div>
-    </Container>
-    
+      </Container>
     </>
-  )
+  );
 }
